c22f3a83626b42fb30ad52d736101c09048c865a
weynard02 <samsicoweynard@gmail.com>
2023-06-22 00:49:50 +0700

    finish condition

--- FP GRAFKOM/Assets/PlayerCollision.cs
+++ FP GRAFKOM/Assets/PlayerCollision.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
     public AudioSource playSound;
+    public AudioSource mainTheme;
+    public AudioSource win;
+
+    public TMP_Text scoreText;
+
+    private int scoreNum;
+
+    public GameObject complete;
+
+    public PlayerMovement movement;
+
+
+    void Start() {
+        scoreNum = 0;
+        scoreText.text = scoreNum + "/3";
+    }
+
     void OnCollisionEnter (Collision collisionInfo) {
 
         if (collisionInfo.collider.tag == "Obstacle") {
@@ -15,6 +34,15 @@ public class PlayerCollision : MonoBehaviour
         else if (collisionInfo.collider.tag == "Coin") {
             playSound.Play();
             Destroy(collisionInfo.gameObject);
+            scoreNum+=1;
+            scoreText.text = scoreNum + "/3";
+        }
+
+        else if (collisionInfo.collider.tag == "Finish" && !complete.activeSelf) {
+            complete.SetActive(true);
+            mainTheme.Stop();
+            win.Play();
+            movement.enabled = false;
         }
     }
 }
